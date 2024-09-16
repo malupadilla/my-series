218f2bd0e00f866f7ff672bb7b57e178b0455df7
malupadilla <145455169+malupadilla@users.noreply.github.com>
2024-09-16 11:01:46 -0300

    llll

--- Src/javascript/script.js
+++ Src/javascript/script.js
@@ -1,1 +1,36 @@
-r
+let count = 0;
+
+// Verifica se existe um valor armazenado para 'count' no Local Storage
+if (localStorage.getItem('count3')) {
+    count = parseInt(localStorage.getItem('count3'));
+}
+
+// Atualiza o elemento HTML com o valor de 'count' ao carregar a página
+const value = document.getElementById('value');
+const plusButton = document.getElementById('plus');
+const minusButton = document.getElementById('minus');
+const resetButton = document.getElementById('reset');
+
+const updateValue = () => {
+    value.innerHTML = count;
+};
+
+updateValue(); /* Clique Normal */
+
+plusButton.addEventListener('click', () => {
+    count += 1;
+    updateValue();
+    localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
+});
+
+minusButton.addEventListener('click', () => {
+    count -= 1;
+    updateValue();
+    localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
+});
+
+resetButton.addEventListener('click', () => {
+    count = 0;
+    updateValue();
+    localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
+});
