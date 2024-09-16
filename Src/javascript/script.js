8e21606b33361605e286842dd3ea6840cf57cf2f
malupadilla <145455169+malupadilla@users.noreply.github.com>
2024-09-16 14:59:02 -0300

    Atualizar o script.js
    gggggg

--- Src/javascript/script.js
+++ Src/javascript/script.js
@@ -1,6 +1,6 @@
 let count = 0;
 
-// Verifica se existe um valor armazenado para 'count' no Local Storage
+// Verifica se existe um valor armazenado para 'count3' no Local Storage
 if (localStorage.getItem('count3')) {
     count = parseInt(localStorage.getItem('count3'));
 }
@@ -20,17 +20,17 @@ updateValue(); /* Clique Normal */
 plusButton.addEventListener('click', () => {
     count += 1;
     updateValue();
-    localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
+    localStorage.setItem('count3', count); // Armazena o valor atualizado no Local Storage
 });
 
 minusButton.addEventListener('click', () => {
     count -= 1;
     updateValue();
-    localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
+    localStorage.setItem('count3', count); // Armazena o valor atualizado no Local Storage
 });
 
 resetButton.addEventListener('click', () => {
     count = 0;
     updateValue();
-    localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
+    localStorage.setItem('count3', count); // Armazena o valor atualizado no Local Storage
 });
